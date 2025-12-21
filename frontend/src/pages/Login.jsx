7525aaa95d7a6086aa35a8b8a78243339f965e75
Colin <77927027+colin-110@users.noreply.github.com>
2025-12-22 00:23:22 +0530

    backend endpount changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/auth/login", {
+      const response = await fetch("https://job-tracker-m2pb.onrender.com//auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -11,7 +11,7 @@ function Signup() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/auth/signup", {
+      const response = await fetch("https://job-tracker-m2pb.onrender.com//auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
